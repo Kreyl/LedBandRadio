3239147af877f83377dfe87e80f6ccff8b54d885
Kreyl <laystoll@yandex.ru>
2016-05-09 23:55:57 +0300

    PCB ready for production

---
+++ LedBandRadio.mm
@@ -0,0 +1,79 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="LedBandRadio" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1462772960915"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="MCU" POSITION="right" ID="ID_785277233" CREATED="1462772996851" MODIFIED="1462773000585">
+<node TEXT="stm32L151" ID="ID_76788501" CREATED="1462773001638" MODIFIED="1462773045489">
+<node TEXT="&#x418;&#x445; &#x43c;&#x43d;&#x43e;&#x433;&#x43e;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1223155080" CREATED="1462773047024" MODIFIED="1462773053576"/>
+<node TEXT="&#x41d;&#x443;&#x436;&#x435;&#x43d; &#x43a;&#x432;&#x430;&#x440;&#x446; &#x434;&#x43b;&#x44f; USB" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1968227926" CREATED="1462773054141" MODIFIED="1462773066780"/>
+<node TEXT="&#x420;&#x430;&#x437;&#x432;&#x435;&#x434;&#x451;&#x43c; &#x43a;&#x432;&#x430;&#x440;&#x446;. &#x414;&#x43b;&#x44f; &#x44d;&#x442;&#x43e;&#x439; &#x437;&#x430;&#x434;&#x430;&#x447;&#x438; &#x43e;&#x43d; &#x43d;&#x430;&#x43c; &#x43d;&#x435; &#x43d;&#x443;&#x436;&#x435;&#x43d;." ID="ID_1864653271" CREATED="1462773087534" MODIFIED="1462773104704"/>
+</node>
+</node>
+<node TEXT="&#x41f;&#x438;&#x442;&#x430;&#x43d;&#x438;&#x435;" POSITION="right" ID="ID_1570648772" CREATED="1462773109870" MODIFIED="1462773114543">
+<node TEXT="&#x420;&#x430;&#x437;&#x44a;&#x451;&#x43c;" ID="ID_1961987923" CREATED="1462773142659" MODIFIED="1462773146289">
+<node TEXT="&#x411;&#x43e;&#x447;&#x43a;&#x430;" STYLE_REF="NotOk" ID="ID_931177176" CREATED="1462773147352" MODIFIED="1462774529156">
+<node TEXT="&#x41d;&#x435; &#x444;&#x438;&#x43a;&#x441;&#x438;&#x440;&#x443;&#x435;&#x442;&#x441;&#x44f;" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1596474964" CREATED="1462773290560" MODIFIED="1462773301215"/>
+<node TEXT="&#x41d;&#x435;&#x443;&#x434;&#x43e;&#x431;&#x43d;&#x43e; &#x437;&#x430;&#x436;&#x438;&#x43c;&#x430;&#x442;&#x44c; &#x43f;&#x440;&#x43e;&#x432;&#x43e;&#x434;&#x430;" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_622587829" CREATED="1462773302061" MODIFIED="1462773318528"/>
+</node>
+<node TEXT="&#x41c;&#x438;&#x43d;&#x438;&#x444;&#x438;&#x442;" ID="ID_99547120" CREATED="1462773149923" MODIFIED="1462773154429">
+<node TEXT="&#x424;&#x438;&#x43a;&#x441;&#x438;&#x440;&#x443;&#x435;&#x442;&#x441;&#x44f;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1504110062" CREATED="1462773266771" MODIFIED="1462773272560"/>
+<node TEXT="&#x41d;&#x435;&#x443;&#x434;&#x43e;&#x431;&#x43d;&#x43e; &#x437;&#x430;&#x436;&#x438;&#x43c;&#x430;&#x442;&#x44c; &#x43f;&#x440;&#x43e;&#x432;&#x43e;&#x434;&#x430;" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1637345954" CREATED="1462773273047" MODIFIED="1462773285758"/>
+</node>
+<node TEXT="&#x41a;&#x43b;&#x435;&#x43c;&#x43c;&#x43d;&#x438;&#x43a;" ID="ID_1632098667" CREATED="1462773155051" MODIFIED="1462773159552">
+<node TEXT="&#x420;&#x430;&#x437;&#x44a;&#x451;&#x43c;&#x43d;&#x44b;&#x439;" ID="ID_851839526" CREATED="1462773166878" MODIFIED="1462773177347">
+<node TEXT="&#x41d;&#x435; &#x444;&#x438;&#x43a;&#x441;&#x438;&#x440;&#x443;&#x435;&#x442;&#x441;&#x44f;" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_229720810" CREATED="1462773217017" MODIFIED="1462773264746"/>
+<node TEXT="&#x423;&#x434;&#x43e;&#x431;&#x43d;&#x43e; &#x437;&#x430;&#x436;&#x430;&#x442;&#x44c; &#x43b;&#x44e;&#x431;&#x44b;&#x435; &#x43f;&#x440;&#x43e;&#x432;&#x43e;&#x434;&#x430;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1053520145" CREATED="1462773242413" MODIFIED="1462773253577"/>
+</node>
+<node TEXT="&#x41d;&#x430;&#x436;&#x438;&#x43c;&#x43d;&#x43e;&#x439;" STYLE_REF="NotOk" ID="ID_1436438973" CREATED="1462773177883" MODIFIED="1462773214127">
+<node TEXT="&#x41d;&#x435;&#x443;&#x434;&#x43e;&#x431;&#x43d;&#x43e; &#x43c;&#x435;&#x43d;&#x44f;&#x442;&#x44c; &#x430;&#x43a;&#x43a;&#x443;&#x43c;" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_313367072" CREATED="1462773183050" MODIFIED="1462773209232"/>
+<node TEXT="&#x41c;&#x430;&#x445;&#x440;&#x44f;&#x442;&#x441;&#x44f; &#x438; &#x437;&#x430;&#x43c;&#x44b;&#x43a;&#x430;&#x44e;&#x442;&#x441;&#x44f; &#x43f;&#x440;&#x43e;&#x432;&#x43e;&#x434;&#x430;" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_450939326" CREATED="1462773198618" MODIFIED="1462773209594"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
